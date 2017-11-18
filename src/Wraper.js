499c8790b3a0df48fd881ce91dc64b3519b2fd21
wolyshaw <wolyshaw@users.noreply.github.com>
2017-11-18 21:58:11 +0800

    fix style

--- src/Wraper.js
+++ src/Wraper.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react'
 
 
 let wraperStyle = { display:'flex', position: 'fixed', overflow: 'hidden', top: 0, right: 0, bottom: 0, left: 0, backgroundColor: 'rgba(0, 0, 0, .5)', zIndex: '1' }
-let containerStyle = { overflowY: 'auto', margin: 'auto' }
+let containerStyle = { display:'flex', overflowY: 'auto', width: '100%', maxHeight: '100%' }
 
 const Mask = props => {
   return (
@@ -17,9 +17,9 @@ export default class Wraper extends PureComponent {
 
   render() {
     return (
-      <div className={ this.props.className }>
-        <span className='float-container' style={ wraperStyle } onClick={ this.props.close }>
-          <span onClick={ e => e.stopPropagation() } style={ containerStyle }>{ this.props.children }</span>
+      <div className={ this.props.className } style={ wraperStyle }>
+        <span className='float-container' onClick={ this.props.close } style={ containerStyle }>
+          <span onClick={ e => e.stopPropagation() } style={{ margin: 'auto'}}>{ this.props.children }</span>
         </span>
       </div>
     )
