073f41da8c2fdb7b729e9dd729c0781a6d8c7f9b
wolyshaw <wolyshaw@users.noreply.github.com>
2017-11-26 20:52:53 +0800

    add build file

---
+++ dist/store.js
@@ -0,0 +1,44 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var store = function () {
+  function store(float) {
+    _classCallCheck(this, store);
+
+    this.float = float || {};
+    this.openlist = {};
+  }
+
+  _createClass(store, [{
+    key: "getStoreByName",
+    value: function getStoreByName(name) {
+      return this.openlist[name];
+    }
+  }, {
+    key: "getComponentByName",
+    value: function getComponentByName(name) {
+      return this.float[name];
+    }
+  }, {
+    key: "add",
+    value: function add(float) {
+      this.openlist[float.name] = float;
+    }
+  }, {
+    key: "remove",
+    value: function remove(name) {
+      delete this.openlist[name];
+    }
+  }]);
+
+  return store;
+}();
+
+exports.default = store;
